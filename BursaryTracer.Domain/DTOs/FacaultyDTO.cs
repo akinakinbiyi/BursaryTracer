c583285b351577caf9ad95cee3875d3201024637
akinakinbiyi <akin.akinbiyi@gmail.com>
2019-04-29 20:30:18 -0500

    ik

---
+++ BursaryTracer.Domain/DTOs/FacaultyDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BursaryTracer.Domain.DTOs
+{
+  public  class FacaultyDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
